b6832f0ab60f8b1c81c88fc8350eddcef8c74b57
shumman <shumman2007@hotmail.com>
2024-06-13 01:12:07 +0500

    pushing files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
+import Image  from "next/image";
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 export default function Home() {
   return (
     <>
       <div className="flex flex-col ">
         <div className="min-h-[100vh] relative">
-          <Image
+          <Image 
+          priority
             className="absolute top-0 h-full w-full"
             style={{ opacity: "0.2" }}
             src={"/images/home/home-banner.png"}
@@ -13,8 +14,8 @@ export default function Home() {
             width={1000}
             height={500}
           />
-          <div className="pt-56 flex gap-4 px-28 pb-16 justify-center">
-            <div className="flex flex-col gap-4 w-1/2 justify-center">
+          <div className="pt-72 sm:pt-72 md:pt-56   flex md:flex-nowrap flex-wrap gap-4 px-8 sm:px-28 pb-16 justify-center">
+            <div className="flex flex-col gap-4 w-full md:w-1/2 justify-center">
               <p style={{ fontFamily: "VoiganteDisplay" }} className="text-5xl">
                 Treasure Hunt through Nashville Breweries
               </p>
@@ -24,7 +25,8 @@ export default function Home() {
                 experience is everything you hoped it would be, and then some.
               </p>
               <button className="relative h-[3rem] self-start flex justify-center items-center p-8">
-                <Image
+                <Image 
+                priority
                   className="w-full h-full absolute top-0 w-full h-full z-[-1]"
                   src={"/images/button/Frame.svg"}
                   alt="button Frame 1"
@@ -34,9 +36,10 @@ export default function Home() {
                 <p>Explore Packages</p>
               </button>
             </div>
-            <div className="w-1/2">
+            <div className="w-full md:w-1/2">
               <div className="h-[28rem] w-full">
-                <Image
+                <Image 
+                priority
                   className="w-full h-full"
                   src={"/images/home/home-img1.svg"}
                   alt="home img1"
@@ -47,8 +50,9 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="px-28 flex flex-col items-center gap-8 relative">
-          <Image
+        <div className="px-8 sm:px-28 flex flex-col items-center gap-8 relative">
+          <Image 
+          priority
             className="absolute w-full h-full z-[-1]"
             style={{ opacity: "0.2" }}
             src={"/images/home/featured-package.png"}
@@ -149,8 +153,8 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="flex gap-4 px-28 mt-16">
-          <div className="w-1/2 flex flex-col gap-4 items-center justify-center">
+        <div className="flex justify-between flex-wrap gap-4 px-8 sm:px-28 mt-16">
+          <div className="w-full lg:w-[40%]  flex flex-col gap-4 items-center justify-center">
             <div className="flex items-center gap-4 self-start">
               <div className="h-[0.5rem] rounded-full w-[5rem] bg-orange-400"></div>
               <p>About Us</p>
@@ -173,7 +177,8 @@ export default function Home() {
                 experience is everything you hoped it would be, and then some. ​
               </p>
               <button className="relative h-[3rem] self-start flex justify-center items-center mt-4 p-8">
-                <Image
+                <Image 
+                priority
                   className="w-full h-full absolute top-0 w-full h-full z-[-1]"
                   src={"/images/button/Frame.svg"}
                   alt="button Frame 1"
@@ -184,9 +189,10 @@ export default function Home() {
               </button>
             </div>
           </div>
-          <div className="w-1/2">
+          <div className="w-full lg:w-[40%] ">
             <div className="h-[25rem]">
-              <Image
+              <Image 
+              priority
                 className="w-full h-full"
                 src={"/images/home/aboutus.svg"}
                 alt="about us"
@@ -198,25 +204,28 @@ export default function Home() {
         </div>
         <div className="flex flex-col mt-16">
           <div className="flex flex-col gap-8">
-            <div className="px-28 flex items-center gap-4 self-start">
+            <div className="px-8 sm:px-28 flex items-center gap-4 self-start">
               <div className="h-[0.5rem] rounded-full w-[5rem] bg-orange-400"></div>
               <p>Testimonials</p>
               <div className="h-[0.5rem] rounded-full w-[5rem] bg-orange-400"></div>
             </div>
-            <div className="px-28 flex gap-8 justify-between">
+            <div className="px-8 sm:px-28 flex gap-8 justify-between">
               <h1 className={`text-4xl `} style={{ fontFamily: "VoiganteDisplay" }}>What Client Say About Us</h1>
               <div className="flex gap-4">
                 <button className="min-h-[3rem] w-[3rem]">
-                  <Image className="w-full h-full" priority src={'/images/layout/forwardButton.svg'} alt="back button" width={100} height={100} />
+                  <Image 
+                  priority className="w-full h-full"  src={'/images/layout/forwardButton.svg'} alt="back button" width={100} height={100} />
                 </button>
                 <button className="min-h-[3rem] w-[3rem]">
-                  <Image className="w-full h-full" priority src={'/images/layout/backButton.svg'} alt="back button" width={100} height={100} />
+                  <Image 
+                  priority className="w-full h-full" src={'/images/layout/backButton.svg'} alt="back button" width={100} height={100} />
                 </button>
               </div>
             </div>
-            <div className="flex gap-16 ">
-              <div className="w-[40%] min-h-[15rem] relative" >
-                <Image
+            <div className="flex  overflow-x-hidden gap-16 ">
+              <div className="w-[81%] min-h-[20rem] max-h-[55rem] relative" >
+                <Image 
+                priority
                   className="w-full h-full absolute top-0 z-[-1]"
                   src={"/images/layout/testimonialBox.png"}
                   alt="about us"
@@ -226,7 +235,8 @@ export default function Home() {
                 <div className="flex flex-col w-full p-16">
                   <div className="flex justify-between items-center gap-4 mb-4">
                     <div className="w-[5rem] h-[3rem]">
-                      <Image
+                      <Image 
+                      priority
                         className="w-full h-full"
                         src={"/images/layout/person.png"}
                         alt="client"
@@ -239,7 +249,8 @@ export default function Home() {
                       <p>Customer</p>
                     </div>
                     <div className="w-[9rem] h-[4rem]">
-                      <Image
+                      <Image 
+                      priority
                         className="w-full h-full"
                         src={"/images/layout/stars.png"}
                         alt="client"
@@ -251,8 +262,9 @@ export default function Home() {
                   <p className="w-full">I remember having treasure hunts as a child and how much fun it was to figure out all of the clues.  Some were simple, while others took a bit to puzzle out.  I can honestly say I don't remember any prizes.  The journey on the way was the real adventure.</p>
                 </div>
               </div>
-              <div className="w-[40%] min-h-[15rem] relative" >
-                <Image
+              <div className="w-[81%] min-h-[20rem] max-h-[55rem] relative" >
+                <Image 
+                priority
                   className="w-full h-full absolute top-0 z-[-1]"
                   src={"/images/layout/testimonialBox.png"}
                   alt="about us"
@@ -262,7 +274,8 @@ export default function Home() {
                 <div className="flex flex-col w-full p-16">
                   <div className="flex justify-between items-center gap-4 mb-4">
                     <div className="w-[5rem] h-[3rem]">
-                      <Image
+                      <Image 
+                      priority
                         className="w-full h-full"
                         src={"/images/layout/person.png"}
                         alt="client"
@@ -275,7 +288,8 @@ export default function Home() {
                       <p>Customer</p>
                     </div>
                     <div className="w-[9rem] h-[4rem]">
-                      <Image
+                      <Image 
+                      priority
                         className="w-full h-full"
                         src={"/images/layout/stars.png"}
                         alt="client"
@@ -287,8 +301,9 @@ export default function Home() {
                   <p className="w-full">I remember having treasure hunts as a child and how much fun it was to figure out all of the clues.  Some were simple, while others took a bit to puzzle out.  I can honestly say I don't remember any prizes.  The journey on the way was the real adventure.</p>
                 </div>
               </div>
-              <div className="w-[40%] min-h-[15rem] relative" >
-                <Image
+              <div className="w-[81%] min-h-[20rem] max-h-[55rem] relative" >
+                <Image 
+                priority
                   className="w-full h-full absolute top-0 z-[-1]"
                   src={"/images/layout/testimonialBox.png"}
                   alt="about us"
@@ -298,7 +313,8 @@ export default function Home() {
                 <div className="flex flex-col w-full p-16">
                   <div className="flex justify-between items-center gap-4 mb-4">
                     <div className="w-[5rem] h-[3rem]">
-                      <Image
+                      <Image 
+                      priority
                         className="w-full h-full"
                         src={"/images/layout/person.png"}
                         alt="client"
@@ -311,7 +327,8 @@ export default function Home() {
                       <p>Customer</p>
                     </div>
                     <div className="w-[9rem] h-[4rem]">
-                      <Image
+                      <Image 
+                      priority
                         className="w-full h-full"
                         src={"/images/layout/stars.png"}
                         alt="client"
@@ -323,8 +340,9 @@ export default function Home() {
                   <p className="w-full">I remember having treasure hunts as a child and how much fun it was to figure out all of the clues.  Some were simple, while others took a bit to puzzle out.  I can honestly say I don't remember any prizes.  The journey on the way was the real adventure.</p>
                 </div>
               </div>
-              <div className="w-[40%] min-h-[15rem] relative" >
-                <Image
+              <div className="w-[81%] min-h-[20rem] max-h-[55rem] relative" >
+                <Image 
+                priority
                   className="w-full h-full absolute top-0 z-[-1]"
                   src={"/images/layout/testimonialBox.png"}
                   alt="about us"
@@ -334,7 +352,8 @@ export default function Home() {
                 <div className="flex flex-col w-full p-16">
                   <div className="flex justify-between items-center gap-4 mb-4">
                     <div className="w-[5rem] h-[3rem]">
-                      <Image
+                      <Image 
+                      priority
                         className="w-full h-full"
                         src={"/images/layout/person.png"}
                         alt="client"
@@ -347,7 +366,8 @@ export default function Home() {
                       <p>Customer</p>
                     </div>
                     <div className="w-[9rem] h-[4rem]">
-                      <Image
+                      <Image 
+                      priority
                         className="w-full h-full"
                         src={"/images/layout/stars.png"}
                         alt="client"
@@ -363,21 +383,24 @@ export default function Home() {
           </div>
         </div>
         <div className="relative min-h-[30rem] mt-16 ">
-          <Image priority className="w-full h-full absolute" style={{opacity:'0.3'}} src={'/images/home/mystery.png'} alt="mystery" width={500} height={700} />
-          <div className="px-32 py-8 mt-16">
+          <Image 
+          priority  className="w-full h-full absolute" style={{opacity:'0.3'}} src={'/images/home/mystery.png'} alt="mystery" width={500} height={700} />
+          <div className="px-8 sm:px-32 py-8 mt-16">
             <div className="relative min-h-[6rem] p-2">
-              <Image priority className="w-full h-full absolute z-[1]" src={'/images/home/mystery-nav.png'} alt="mystery nav" width={300} height={300} />
+              <Image 
+               priority className="w-full h-full absolute z-[1]" src={'/images/home/mystery-nav.png'} alt="mystery nav" width={300} height={300} />
               <div className="mt-8 flex flex-col px-16 pb-4 gap-4">
                 <div className="flex items-center gap-4  relative z-[4]">
                   <div className="min-h-[0.5rem] rounded-full w-[5rem] bg-orange-400"></div>
                   <p style={{ fontFamily: "VoiganteDisplay" }}>Packages</p>
                   <div className="min-h-[0.5rem] rounded-full w-[5rem] bg-orange-400"></div>
                 </div>
-                <div className="z-[4] relative flex justify-between">
-                  <p className="text-[2.2rem]" style={{fontFamily:'VoiganteDisplay'}}>Join the Ultimate Treasure Hunt Adventure!</p>
-                  <button className="relative h-[3rem]  flex justify-center items-center px-20 py-2 box-border">
-                    <Image
-                      className="w-full h-full absolute top-0 w-full h-full z-[-1]"
+                <div className="z-[4] relative flex flex-wrap gap-8 sm:gap-0 sm:justify-between">
+                  <p className="text-[1.5rem] sm:text-[2.2rem]" style={{fontFamily:'VoiganteDisplay'}}>Join the Ultimate Treasure Hunt Adventure!</p>
+                  <button className="relative h-[3rem]  flex justify-center items-center px-8 py-4 sm:px-20 sm:py-2 box-border">
+                    <Image 
+                    priority
+                      className="w-full h-full absolute top-0 w-full h-full z-[-1] sm:object-contain object-cover"
                       src={"/images/button/Frame.svg"}
                       alt="button Frame 1"
                       width={50}
@@ -389,10 +412,11 @@ export default function Home() {
               </div>
             </div>
            
-            <div className="p-8 flex gap-8 justify-center">
+            <div className="p-8 flex flex-wrap sm:flex-nowrap justify-between sm:justify-start gap-8 justify-center">
               <div className="w-1/2 flex flex-col gap-4">
                 <div className="min-h-[5rem] w-[5rem]">
-                  <Image priority className="w-full h-full" src={'/images/layout/riddleNavbar.png'} alt="mystery nav" width={300} height={300} />
+                  <Image 
+                  priority className="w-full h-full" src={'/images/layout/riddleNavbar.png'} alt="mystery nav" width={300} height={300} />
                 </div>
                 <p className="text-sm">That is the inspiration for my Treasure Hunts.  I want to bring joy to others while experiencing joy myself.  I trust your experience is everything you hoped it would be, and then some.</p>
                 <div className="flex gap-4">
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -7,9 +7,34 @@ export default function Navbar() {
         className="absolute flex justify-center w-full top-[2rem]"
         style={{ fontFamily: "VoiganteDisplay" }}
       >
+        <div style={{ background: 'url("/images/layout/navbar.png")',backgroundRepeat:'no-repeat',backgroundSize:'cover' }}
+          className="flex flex-col xl:hidden gap-4  px-8 min-w-[85%] py-4 m-auto items-center justify-center">
+          <div className="flex justify-center">
+            <Image
+              src={"/images/layout/riddleNavbar.png"}
+              alt="riddle logo"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="flex gap-4 flex-wrap justify-center ">
+            <div className="flex gap-16 items-center">
+              <p>Home</p>
+              <p>Packages</p>
+              <p>Breweries</p>
+              <p>Treasure Hunt</p>
+            </div>
+            <div className="flex gap-16 items-center">
+              <p>Riddles</p>
+              <p>Booking</p>
+              <p>About</p>
+              <p>Contact</p>
+            </div>
+          </div>
+        </div>
         <div
           style={{ background: 'url("/images/layout/navbar.png")' }}
-          className="flex gap-16 px-8 min-w-[85%] py-4 m-auto items-center justify-center"
+          className="hidden gap-16 px-8 xl:flex min-w-[85%] py-4 m-auto items-center justify-center"
         >
           <div className="flex gap-16 items-center">
             <p>Home</p>
