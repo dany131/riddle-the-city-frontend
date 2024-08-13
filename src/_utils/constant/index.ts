5e54d9aa72918f265205b67433c99ab46e27b38a
Danyal Nasir <danyalnasir60@gmail.com>
2024-08-13 23:46:38 +0500

    Admin login form validation

---
+++ src/_utils/constant/index.ts
@@ -0,0 +1,9 @@
+export const RegexConstants = {
+  ONLY_LETTERS_SPACES: /^[A-Za-z\s]+$/,
+  PASSWORD: /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,30}$/,
+  YEAR: /^\d{4}$/,
+  LOCATION: {
+    LATITUDE: /^\(?[+-]?(90(\.0+)?|[1-8]?\d(\.\d+)?)$/,
+    LONGITUDE: /^\s?[+-]?(180(\.0+)?|1[0-7]\d(\.\d+)?|\d{1,2}(\.\d+)?)\)?$/,
+  },
+};
--- src/app/(admin)/admin/(auth)/login/page.tsx
+++ src/app/(admin)/admin/(auth)/login/page.tsx
@@ -1,40 +1,51 @@
 'use client';
+
 import axiosInstance from "@/app/utils/axiosInstance";
-import { Checkbox, Input, Radio, RadioGroup } from "@nextui-org/react"
-import Image from "next/image"
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
-import { BsEyeFill, BsEyeSlash } from "react-icons/bs";
-import { CiMail } from "react-icons/ci";
-import { IoIosLock } from "react-icons/io";
-import { useMutation } from "react-query";
-import Cookies from 'js-cookie'
-import { ImSpinner2 } from "react-icons/im";
-import { toast } from "react-toastify";
+import {Input} from "@nextui-org/react";
+import {useRouter} from "next/navigation";
+import {FormEvent, useState} from "react";
+import {BsEyeFill, BsEyeSlash} from "react-icons/bs";
+import {CiMail} from "react-icons/ci";
+import {IoIosLock} from "react-icons/io";
+import {useMutation} from "react-query";
+import Cookies from 'js-cookie';
+import {ImSpinner2} from "react-icons/im";
+import {toast} from "react-toastify";
+import {RegexConstants} from "@/_utils/constant";
+
+
 type LoginData = {
     email: string,
     password: string,
     accessType: number,
     accessToken: string
 }
+
 export default function Login() {
-    const [isVisible, toggleVisibility] = useState(false)
-    const router = useRouter()
-    const [invalid, setInvalid] = useState(false)
-    const [message, setMessage] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [isVisible, toggleVisibility] = useState(false);
+    const router = useRouter();
+    const [invalid, setInvalid] = useState(false);
+    const [message, setMessage] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
     const loginMutation = useMutation((data: LoginData): any => axiosInstance.post('/riddle/api/auth/login', data), {
         onSuccess(data: any) {
-            if (data.data.data.user.role == 'Admin') {
-                setInvalid(false)
-                Cookies.set('accessToken', data.data.data.tokens.access_token)
-                Cookies.set('refreshToken', data.data.data.tokens.refresh_token)
-                Cookies.set('userData', JSON.stringify({ name: data.data.data.user.name, email: data.data.data.user.email, phone: data.data.data.user.phone, role: data.data.data.user.role, id: data.data.data.user._id, profile: data.data.data.user.profilePicture }))
-                router.push('/admin/dashboard')
-            }
-            else {
+            if (data.data.data.user.role === 'Admin') {
+                setInvalid(false);
+                Cookies.set('accessToken', data.data.data.tokens.access_token);
+                Cookies.set('refreshToken', data.data.data.tokens.refresh_token);
+                Cookies.set('userData', JSON.stringify({
+                    name: data.data.data.user.name,
+                    email: data.data.data.user.email,
+                    phone: data.data.data.user.phone,
+                    role: data.data.data.user.role,
+                    id: data.data.data.user._id,
+                    profile: data.data.data.user.profilePicture
+                }));
+                router.push('/admin/dashboard');
+            } else {
                 toast.error('Invalid Credentials', {
                     position: "top-right",
                     autoClose: 5000,
@@ -43,59 +54,51 @@ export default function Login() {
                     pauseOnHover: true,
                     draggable: true,
                     progress: undefined,
-                    theme: "light",
-                })
-                // setInvalid(true)
-                // setMessage('Invalid Credentials')
-            }
-            
-            console.log(data)
-        },
-        onError(error:any) {
-            // setInvalid(true)
-            // setMessage(error.response.data.message)
-            if (typeof (error.response.data.message) == 'string') {
-                toast.error(error.response.data.message, {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                })
-            }
-            else {
-                toast.error(error.response.data.message.join(','), {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                })
+                    theme: "light"
+                });
             }
         },
-    })
+        onError(error: any) {
+            const errorMessage = typeof error.response.data.message === 'string'
+                ? error.response.data.message
+                : error.response.data.message.join(',');
 
-    function handleSubmit(e:FormEvent) {
-        e.preventDefault()
-        const form = e.target as any as HTMLFormElement
-        const formData = new FormData(form)
-        const dataForLogin:LoginData = {
-            email: formData.get('email') as any as string,
-            password: formData.get('password') as any as string,
-            accessType: 1,
-            accessToken:''
+            toast.error(errorMessage, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light"
+            });
         }
-        loginMutation.mutate(dataForLogin)
+    });
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        if (!RegexConstants.PASSWORD.test(password)) {
+            setPasswordError('Password must be 8-30 characters long, include at least one uppercase letter and one number.');
+            return;
+        }
+
+        const form = e.target as HTMLFormElement;
+        const formData = new FormData(form);
+        const dataForLogin: LoginData = {
+            email: formData.get('email') as string,
+            password: formData.get('password') as string,
+            accessType: 1,
+            accessToken: ''
+        };
+
+        setPasswordError('');
+        loginMutation.mutate(dataForLogin);
     }
+
     return (
         <>
-            <form onSubmit={handleSubmit} className="h-full w-full items-center  flex flex-col gap-4 p-8 sm:px-24">
+            <form onSubmit={handleSubmit} className="h-full w-full items-center flex flex-col gap-4 p-8 sm:px-24">
                 <h1 className="text-2xl font-bold">Welcome Back!</h1>
                 {invalid && <p className="text-red-600">{message}</p>}
                 <p>Please use your email and password to login</p>
@@ -104,39 +107,41 @@ export default function Login() {
                     name="email"
                     className="w-full"
                     type="email"
-                    isInvalid={email == '' && loginMutation.isError}
+                    isInvalid={email === '' && loginMutation.isError}
                     errorMessage="Please Enter Email"
                     onChange={(e) => {
-                        setEmail(e.target.value)
+                        setEmail(e.target.value);
                     }}
                     label="Email"
                     placeholder="you@example.com"
                     labelPlacement="outside"
                     startContent={
-                        <CiMail className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                        <CiMail className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
                     }
                 />
                 <Input
                     name="password"
                     required
-                    isInvalid={password == '' && loginMutation.isError}
-                    errorMessage="Please Enter Password"
+                    isInvalid={passwordError !== ''}
+                    errorMessage={passwordError}
                     onChange={(e) => {
-                        setPassword(e.target.value)
+                        setPassword(e.target.value);
                     }}
                     label="Password"
-                    className={'w-full'}
+                    className="w-full"
                     placeholder="Enter your password"
                     labelPlacement="outside"
                     startContent={
-                        <IoIosLock className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                        <IoIosLock className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
                     }
                     endContent={
-                        <button className="focus:outline-none" type="button" onClick={()=>{toggleVisibility(!isVisible)}}>
+                        <button className="focus:outline-none" type="button" onClick={() => {
+                            toggleVisibility(!isVisible);
+                        }}>
                             {isVisible ? (
-                                <BsEyeSlash className="text-2xl text-default-400 pointer-events-none" />
+                                <BsEyeSlash className="text-2xl text-default-400 pointer-events-none"/>
                             ) : (
-                                    <BsEyeFill className="text-2xl text-default-400 pointer-events-none" />
+                                <BsEyeFill className="text-2xl text-default-400 pointer-events-none"/>
                             )}
                         </button>
                     }
@@ -147,8 +152,10 @@ export default function Login() {
                     {/* <Checkbox>Remember Me</Checkbox> */}
                     {/* <Link href={'/admin/forgot-password'} className="text-blue-600 underline">Forgot Password?</Link> */}
                 </div>
-                <button type="submit" className="px-16 py-2 bg-[#A92223] flex justify-center rounded text-white w-max ">{loginMutation.isLoading ? <ImSpinner2 className="text-xl animate-spin" />:"Login" }</button>
+                <button type="submit" className="px-16 py-2 bg-[#A92223] flex justify-center rounded text-white w-max ">
+                    {loginMutation.isLoading ? <ImSpinner2 className="text-xl animate-spin"/> : "Login"}
+                </button>
             </form>
         </>
-    )
+    );
 }
