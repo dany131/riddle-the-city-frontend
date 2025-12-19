de7abe07ee4c0d7a070890e9b91ec61136ee1b3f
haseebbsal1 <haseeb.salman1234@outlook.com>
2025-12-19 07:56:52 +0500

    fix :script

--- src/app/(user)/(home)/layout.tsx
+++ src/app/(user)/(home)/layout.tsx
@@ -1,15 +1,22 @@
 "use client";
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
-import { useEffect } from "react";
+import Script from "next/script";
 export default function UserLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  useEffect(() => {
-    document.querySelector("body")!.innerHTML += `<script>
-!function(f,b,e,v,n,t,s)
+  return (
+    <>
+      <div className="bg-[#160704] text-white overflow-x-hidden">
+        <Navbar />
+        {children}
+        <Footer />
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `        !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -18,19 +25,17 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '1138170934636279');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1138170934636279&ev=PageView&noscript=1"
-/></noscript>
-`;
-  }, []);
-  return (
-    <>
-      <div className="bg-[#160704] text-white overflow-x-hidden">
-        <Navbar />
-        {children}
-        <Footer />
+fbq('track', 'PageView');`,
+          }}
+        ></Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1138170934636279&ev=PageView&noscript=1"
+          />
+        </noscript>
       </div>
     </>
   );
